7af99edce52fecc3a836b3c5bb3f56d8c244fa24
KamilZ121 <wi24b121@technikum-wien.at>
2025-07-08 13:43:47 +0200

    Create class structure

---
+++ src/main/java/Board.java
@@ -0,0 +1,30 @@
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return false;
+    }
+
+    public void place(int x, int y, char marker) {
+
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+
+}
+
+
+
+
+
---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,30 @@
+
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    public TicTacToe() {
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        board = new Board();
+    }
+
+    public void start() {
+
+    }
+
+    public void switchCurrentPlayer() {
+
+    }
+
+    public boolean hasWinner() {
+        return false;
+    }
+}
+
+
+
+
